8bdc56a979487d010e4ad6b809225a38a1c9f9a9
Eugene-000 <gryaznov.z@bk.ru>
2023-02-08 17:01:15 +0400

    Развертывание react-typescript

--- src/components/ButtonFavourite/ButtonFavourite.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './ButtonFavourite.module.scss';
-
-export const ButtonFavourite = ({like}) => {
-  return (
-    <button type="button" className={[styles.favouriteBtn, like ? styles.active : styles.nonActive].join(' ')}></button>
-  )
-}
--- src/constants/routes.js
+++
@@ -1,3 +0,0 @@
-export const SERVER_URL = 'http://localhost:3006';
-export const PATH_INDEX = '/';
-export const PATH_DETAILED = '/:id';
